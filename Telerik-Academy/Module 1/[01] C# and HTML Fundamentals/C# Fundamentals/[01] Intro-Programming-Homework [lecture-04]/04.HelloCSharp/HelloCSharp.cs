581938c020e1667b29e1c7134c46aa9871dc88e5
Vasil Profirov <Vasil.Profirov@gmail.com>
2016-10-20 14:29:47 +0300

    1st Commit
    Hopefult all of this commits without problems
    TODO: Create develop branch

---
+++ Telerik-Academy/Module 1/[01] C# and HTML Fundamentals/C# Fundamentals/[01] Intro-Programming-Homework [lecture-04]/04.HelloCSharp/HelloCSharp.cs
@@ -0,0 +1,19 @@
+﻿/*
+Problem 4. Hello World
+
+Create, compile and run a “Hello C#” console application.
+Ensure you have named the application well (e.g. “”HelloCSharp”).
+*/
+
+namespace HelloCSharp
+{
+    using System;
+
+    class HelloCSharp
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello C#");
+        }
+    }
+}
