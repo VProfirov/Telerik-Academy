faf7a9e780cbd66c70a75d6cf10b13720c09ff1b
Vasil Profirov <Vasil.Profirov@gmail.com>
2016-11-01 23:03:42 +0200

    some small formating fix

--- Telerik-Academy/Module 1/[01] C# and HTML Fundamentals/C# Fundamentals/[06]Loops[lecture-09]/04.PrintDeckOf52Cards/PrintDeckOf52Cards.cs
+++ Telerik-Academy/Module 1/[01] C# and HTML Fundamentals/C# Fundamentals/[06]Loops[lecture-09]/04.PrintDeckOf52Cards/PrintDeckOf52Cards.cs
@@ -12,18 +12,19 @@ namespace PrintDeckOf52Cards
     {
         static void Main()
         {
-             string[] deck = new[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", };
-             string card = Console.ReadLine().ToUpper();
-             for (int i = 0; i < deck.Length; i++)
-             {
-             Console.WriteLine("{0} of spades, {0} of clubs, {0} of hearts, {0} of diamonds", deck[i]);
-             if (deck[i] == card)
-             {
-             break;
-             }
-             }
-             ////To use the Hack Literal Solution comment the top-side code and uncomment the one below
-//             TheLitteralHackSolution.HackSolution();
+            string[] deck = new[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", };
+            string card = Console.ReadLine().ToUpper();
+            for (int i = 0; i < deck.Length; i++)
+            {
+                Console.WriteLine("{0} of spades, {0} of clubs, {0} of hearts, {0} of diamonds", deck[i]);
+                if (deck[i] == card)
+                {
+                    break;
+                }
+            }
+
+            ////To use the Hack Literal Solution comment the top-side code and uncomment the one below
+            // TheLitteralHackSolution.HackSolution();
         }
     }
 
