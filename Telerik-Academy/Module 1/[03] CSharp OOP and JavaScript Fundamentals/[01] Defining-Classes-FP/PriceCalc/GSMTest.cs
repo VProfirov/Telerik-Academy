1c68a7e230b8694641c11e0c90ae64797565e320
Vasil Profirov <Vasil.Profirov@gmail.com>
2016-11-20 01:10:47 +0200

    OOp & JS

---
+++ Telerik-Academy/Module 1/[03] CSharp OOP and JavaScript Fundamentals/[01] Defining-Classes-FP/PriceCalc/GSMTest.cs
@@ -0,0 +1,68 @@
+﻿/* Define several constructors for the 
+ * defined classes that take different 
+ * sets of arguments (the full information for the class or part of it). 
+ * Assume that model and manufacturer are mandatory (the others are optional). 
+ * All unknown data fill with null.
+ */
+
+using System;
+using System.Linq;
+
+
+public class GSMTest
+{
+    static void Main()
+    {
+        // GSMTest
+        GSM[] test = new GSM[3];
+
+        Display testDisplay = new Display(12, 13);
+        Battery testBattery = new Battery(BatteryType.LiIon, 10, 10);
+
+        GSM firstPhone = new GSM("test", "test", 12, "Shefa Joro", testBattery, testDisplay);
+        test[0] = firstPhone;
+
+        GSM secondPhone = new GSM("SecondTest", "SecondTest", 14, "Moore Name", testBattery, testDisplay);
+        test[1] = secondPhone;
+
+        GSM thirdPhone = new GSM("Some test", "Texttt", 1, "Name", testBattery, testDisplay);
+        test[2] = thirdPhone;
+
+        for (int i = 0; i < test.Length; i++)
+        {
+            Console.WriteLine(test[i]);
+        }
+
+        Console.WriteLine(GSM.Iphone.Model);
+        Console.WriteLine(GSM.Iphone.Manufacturer);
+        Console.WriteLine(firstPhone.Battery.BatteryModel);
+
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("GSMCallHistoryTest");
+        Console.WriteLine("---------------------------------");
+
+        //GSMCallHistoryTest
+        GSM myPhone = new GSM("Nokia", "Nokia Corp", 1, "Ivan", testBattery, testDisplay);
+
+
+        myPhone.AddCall(DateTime.Now, "088888888", 236);
+        myPhone.AddCall(DateTime.Now, "077777777", 333);
+        myPhone.AddCall(DateTime.Now, "066666666", 123);
+        myPhone.AddCall(DateTime.Now, "055555555", 11);
+        myPhone.AddCall(DateTime.Now, "044444444", 23);
+
+        myPhone.DisplayCallHistory();
+
+        Console.WriteLine(myPhone.CalcPrice(0.37));
+
+        myPhone.RemoveCallByDuration(333);
+
+        myPhone.DisplayCallHistory();
+        Console.WriteLine(myPhone.CalcPrice(0.37));
+
+        myPhone.ClearHistory();
+        myPhone.DisplayCallHistory();
+
+
+    }
+}
