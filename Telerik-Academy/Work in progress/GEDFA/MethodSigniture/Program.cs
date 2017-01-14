01d20d27ca79fdc5a5902ee8cf6456eab3443fb9
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-01-14 23:49:07 +0200

    Exploring init & inh with members and access
    ctor() init + static ctor() =>valid
    ctor() *2nd *: base ctor() =>by default to init inh

---
+++ Telerik-Academy/Work in progress/GEDFA/MethodSigniture/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSigniture
+{
+    using System.Runtime.CompilerServices;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //breaking  methods
+        }
+
+//        public int SomeM1(int x)
+//        {
+//            return x;
+//        }
+//
+//        public int SomeM1(int y)
+//        {
+//            return y;
+//        }
+//
+//        public void ParamsM1(int[] x)
+//        {
+////            return x;
+//        }
+//        public void ParamsM1(params int[] x)
+//        {
+//            
+//        }
+//
+//        public void PM1(params int[]x, int y)
+//        {
+//        }
+    
+    }
+}
+///https://msdn.microsoft.com/en-us/library/aa691131(v=vs.71).aspx
