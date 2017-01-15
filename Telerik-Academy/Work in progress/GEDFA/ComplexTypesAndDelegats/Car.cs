13e2fabafb98974b14eb9665157c3dc62158ae60
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-01-15 05:38:30 +0200

    added

---
+++ Telerik-Academy/Work in progress/GEDFA/ComplexTypesAndDelegats/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexTypesAndDelegats
+{
+    class Car
+    {
+        public Car(string model, string manufacturer)
+        {
+            this.Model = model;
+            this.Manufacturer = manufacturer;
+        }
+        public string Model { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public string iModel()
+        {
+            return $"The Model is : {this.Model}";
+        }
+
+        public string iManufacturer()
+        {
+            return $"The Manufac is : {this.Manufacturer}";
+        }
+
+        public void ModelChange()
+        {
+            this.Model = "E3";
+            this.Manufacturer = "Tesla";
+        }
+    }
+}
---
+++ Telerik-Academy/Work in progress/GEDFA/ComplexTypesAndDelegats/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexTypesAndDelegats
+{
+    using System.Reflection;
+
+    class Program
+    {
+        public delegate string Changer();
+
+        public delegate string Cunt(int lvl, string idiotism);
+
+        public delegate void FU();
+
+        static void Main(string[] args)
+        {
+            Car newCar = new Car("troshka","BWM");
+
+
+            Changer change = new Changer(newCar.iModel);
+            change();
+
+            Program pr = new Program();
+            Cunt cunt = new Cunt(pr.WTF);
+            cunt += pr.WTF;
+            var c = cunt.GetMethodInfo().ToString();
+
+            Console.WriteLine(c);
+            cunt.Invoke(5, "normal");
+
+
+            Cunt cunt2 = pr.WTF;
+            cunt2(10,"mtf");
+
+            FU fuCar = newCar.ModelChange;
+//            fuCar.Invoke(); //== fuCar();
+            fuCar();
+
+            Console.WriteLine(newCar.iModel());
+        }
+
+
+        string WTF(int lvl,string idio)
+        {
+            Console.WriteLine($"Level {lvl} of idiotism:{idio}");
+            return "fu return";
+        }
+
+
+    }
+
+}
