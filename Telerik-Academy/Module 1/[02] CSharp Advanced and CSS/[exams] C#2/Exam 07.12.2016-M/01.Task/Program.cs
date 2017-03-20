fbdf019b073c4b4e05ab5260798078304fbeeb7e
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-03-20 20:29:41 +0200

    before-last

---
+++ Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/01.Task/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+//using System.Linq;
+namespace _01.Task
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long longue0b = 0b1000_0001;
+            long longue = 1000_0001;
+            long l = 129;
+            string L = Convert.ToString(l,2);
+
+            Console.WriteLine("number: " + l);
+            Console.WriteLine("0b: " + longue0b);
+            Console.WriteLine("_: " + longue);
+            Console.WriteLine("Convert.ToString(_,_): "+ L);
+            
+        }
+    }
+}
