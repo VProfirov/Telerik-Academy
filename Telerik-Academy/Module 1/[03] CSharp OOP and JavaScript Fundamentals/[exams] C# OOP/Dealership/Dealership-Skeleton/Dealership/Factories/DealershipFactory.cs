293f39a6d7407a2aecd546d174c90384668b33cf
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-05-28 12:50:18 +0300

    just another commit crash

--- Telerik-Academy/Module 1/[03] CSharp OOP and JavaScript Fundamentals/[exams] C# OOP/Dealership/Dealership-Skeleton/Dealership/Factories/DealershipFactory.cs
+++ Telerik-Academy/Module 1/[03] CSharp OOP and JavaScript Fundamentals/[exams] C# OOP/Dealership/Dealership-Skeleton/Dealership/Factories/DealershipFactory.cs
@@ -2,7 +2,6 @@
 using System;
 using Dealership.Models;
 using Dealership.Common.Enums;
-using Dealership.Models.Vehicles;
 
 namespace Dealership.Factories
 {
@@ -10,27 +9,27 @@ namespace Dealership.Factories
     {
         public IVehicle CreateCar(string make, string model, decimal price, int seats)
         {
-            return new Car(make,model,price,seats);
+            // TODO: Implement this
         }
 
         public IVehicle CreateMotorcycle(string make, string model, decimal price, string category)
         {
-            return new Motorcycle(make,model,price,category);
+            // TODO: Implement this
         }
 
         public IVehicle CreateTruck(string make, string model, decimal price, int weightCapacity)
         {
-            return new Truck(make,model,price,weightCapacity);
+            // TODO: Implement this
         }
 
         public IUser CreateUser(string username, string firstName, string lastName, string password, string role)
         {
-            return new User(username,firstName,lastName,password,role);
+            // TODO: Implement this
         }
 
         public IComment CreateComment(string content)
         {
-            return new Comment(content);
+            // TODO: Implement this
         }
     }
 }
