d3e150cee3f8ab0ee19b9d214aaf3e9298d16756
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-05-29 13:13:38 +0300

    Auto stash before merge of "master" and "origin/master"

---
+++ Telerik-Academy/Module 1/[03] CSharp OOP and JavaScript Fundamentals/[exams] C# OOP/Dealership v0.1/Dealership-Skeleton/Dealership/Models/Output/VehiclesOutput.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dealership.Contracts;
+
+namespace Dealership.Models.Output
+{
+    public static class VehiclesOutput
+    {
+        public static string VehicleGeneralInfo(IVehicle vehicle)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"  Make: {vehicle.Make}");
+            sb.AppendLine($"  Model: {vehicle.Model}");
+            sb.AppendLine($"  Wheels: {vehicle.Wheels}");
+            sb.AppendLine($"  Price: ${vehicle.Price}");
+//            sb.AppendLine(this.PrintSpecialInfo());
+//TODO: FIX !! Common-Named method
+            // VEHICLE TYPE SPECIFIC INFO
+            sb.AppendLine(PrintComments(vehicle.Comments));
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string TruckSpecificInfo(ITruck truck)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"  Weight Capacity: {truck.WeightCapacity}t");
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string MotorcycleSpecificInfo(IMotorcycle motorcycle)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"  Category: {motorcycle.Category}");
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string CarSpecificInfo(ICar car)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"  Seats: {car.Seats}");
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string PrintComments(IList<IComment> comments)
+        {
+            //TODO: RE-CHECK (comments is a field; this is comments...)
+            var builder = new StringBuilder();
+
+            if (comments.Count <= 0)
+            {
+                builder.AppendLine("    --NO COMMENTS--");
+            }
+            else
+            {
+                builder.AppendLine("    --COMMENTS--");
+                builder.AppendLine("    ----------");
+                builder.AppendLine(comments[0].ToString());
+
+                for (int i = 1; i < comments.Count; i++)
+                {
+                    builder.AppendLine("    ----------");
+                    builder.AppendLine("    ----------");
+                    builder.AppendLine(comments[i].ToString());
+                }
+
+                builder.AppendLine("    ----------");
+                builder.AppendLine("    --COMMENTS--");
+            }
+
+            return builder.ToString().TrimEnd();
+        }
+    }
+}
+
