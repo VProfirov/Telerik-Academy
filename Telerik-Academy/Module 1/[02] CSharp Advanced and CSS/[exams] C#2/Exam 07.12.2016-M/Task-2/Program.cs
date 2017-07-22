506f31d639c6401ec4180f7a7131989e1b0232c4
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-07-22 20:16:11 +0300

    02

--- Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/Task-2/Program.cs
+++ Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/Task-2/Program.cs
@@ -2,21 +2,53 @@
 
 namespace Task_2
 {
+    using System.Linq;
+    using System.Text;
+
     class Program
     {
         static void Main(string[] args)
         {
             var count = int.Parse(Console.ReadLine());
-            var hands = new int[count];
+////          var hands = new int[count]; // if we need to use the cards-info
 
             var types = "23456789TJQKA";
             var suits = "cdhs";
 
             var cards = new int[52];
 
-            //goal 
-                //build a deck[52]
-                //even number of occuring cards - in all hands
+            ////goal 
+                ////build a deck[52] - isFullDeck
+                ////even number of occuring cards - in all hands
+
+            while (count-- > 0)
+            {
+                // The Reverse case (array count 0->n VS bit count n <- 0) => process it as collection (IEnumerable)
+                var hand = string.Join(string.Empty, Convert.ToString(long.Parse(Console.ReadLine()), 2).Reverse());
+
+                for (int i = 0; i < hand.Length; i++)
+                {
+                    cards[i] += hand[i] - '0';
+                }
+            }
+
+            ////isFullDeck
+            var hasFullDeck = cards.Where(x => x > 0).Count() == cards.Length;
+            Console.WriteLine(hasFullDeck ? "Full deck" : "Wa wa!");
+
+            ////even N of cards
+            var sb = new StringBuilder();
+            for (int i = 0; i < cards.Length; i++)
+            {
+                if (cards[i] % 2 == 0)
+                {
+                    var typeIndex = i % types.Length;  // index inside a cycle
+                    var suitsIndex = i / types.Length; // integer cycle (integer devision)
+                    sb.Append(types[typeIndex] + "" + suits[suitsIndex] + " ");
+                }
+            }
+            var result = sb.ToString();
+            Console.WriteLine(result);
         }
     }
 }
--- Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/TestLand/Program.cs
+++ Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/TestLand/Program.cs
@@ -1,17 +1,51 @@
 ﻿using System;
+using System.Linq;
 
-namespace TestLand
+class Solution
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            var hand = "1111111111111111111111111100000000000000000000000000";
-//            Console.WriteLine(hand.Length);
-            Console.WriteLine(hand);
-            Console.WriteLine(Convert.ToString(4503599560261632,2));
-//            Console.WriteLine(Convert.ToString(long.MaxValue,2));
+        const string types = "23456789TJQKA";
+        const string suits = "cdhs";
 
+        var n = int.Parse(Console.ReadLine());
+        var cards = new int[52];
+
+        while (n-- > 0)
+        {
+            // the solution doesn't require built-in methods like .Reverse(), same can be achievev using loops
+            var hand = string.Join("", Convert.ToString(long.Parse(Console.ReadLine()), 2).Reverse());
+            var hand2 = string.Join("", Convert.ToString(long.Parse(Console.ReadLine()), 2).Reverse());
+            var hand3 = string.Empty;
+            var hand4 = "";
+
+            var a = "A";
+            var a1 = "A";
+            Console.WriteLine("Internate: " + object.ReferenceEquals(a,a1));
+            Console.WriteLine("Compare: " + string.Compare(hand3,hand4) + "| RefEq:  " + object.ReferenceEquals(hand3,hand4)) ;
+            Console.WriteLine($"HAND: {hand}");
+            Console.WriteLine($"HAND2: {hand2}");
+
+            for (var i = 0; i < hand.Length; i++)
+            {
+                cards[i] += (hand[i] - '0');
+            }
+        }
+
+        // instead of for loop that counts
+        var hasFullDeck = cards.Count(x => x > 0) == cards.Length;
+
+        Console.WriteLine(hasFullDeck ? "Full deck" : "Wa wa!");
+
+        for (var i = 0; i < cards.Length; i++)
+        {
+            if (cards[i] % 2 == 0)
+            {
+                var typeIndex = i % types.Length;
+                var suitIndex = i / types.Length;
+
+                Console.Write(types[typeIndex] + "" + suits[suitIndex] + " ");
+            }
         }
     }
 }
