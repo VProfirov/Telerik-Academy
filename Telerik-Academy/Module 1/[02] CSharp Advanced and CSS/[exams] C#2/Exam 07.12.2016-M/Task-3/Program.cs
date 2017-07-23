622f217b82b11cf4e43bf91296bcb9886dd6d6ef
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-07-23 21:31:28 +0300

    03

--- Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/Task-3/Program.cs
+++ Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/Task-3/Program.cs
@@ -2,11 +2,41 @@
 
 namespace Task_3
 {
+    using System.Text;
+    using System.Text.RegularExpressions;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var pattern = Console.ReadLine().ToUpper();
+            var count = int.Parse(Console.ReadLine());
+            var sb = new StringBuilder();
+            while (count-- > 0)
+            {
+                sb.Append(Console.ReadLine());
+            }
+            var text = sb.ToString().ToUpper();
+            Console.WriteLine(text);
+            var patternIndexInString = text.IndexOf(pattern);
+//            Console.WriteLine(Regex.IsMatch(text,@"[.|?]"));
+            var dot = text.IndexOf(".", patternIndexInString + pattern.Length - 1);
+            var questionMark = text.IndexOf("?", patternIndexInString + pattern.Length - 1);
+            var punctuation = dot > questionMark ? "." : "?"; // use REGEX
+
+            if (punctuation == ".")
+            {
+                
+            }
+            else
+            {
+                
+            }
         }
     }
 }
+/*
+Statement(.) - The substring starts from the beginning of the sentence(inclusive), till the beginning of the keyword(exclusive) we are looking for.
+Question(?) - The substring starts from the end of the keyword(exclusive) we are looking for, till the end of the sentence(without the question mark though).*/
+
+    // result += UpperCase noEmptySpaces
--- Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/TestLand/Program.cs
+++ Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/TestLand/Program.cs
@@ -3,49 +3,30 @@ using System.Linq;
 
 class Solution
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        const string types = "23456789TJQKA";
-        const string suits = "cdhs";
+        Console.WriteLine(PrintN());
+    }
 
-        var n = int.Parse(Console.ReadLine());
-        var cards = new int[52];
+    public static void PrintFinally()
+    {
+        Console.WriteLine("Finally");
+    }
 
-        while (n-- > 0)
+    public static string PrintN()
+    {
+        try
         {
-            // the solution doesn't require built-in methods like .Reverse(), same can be achievev using loops
-            var hand = string.Join("", Convert.ToString(long.Parse(Console.ReadLine()), 2).Reverse());
-            var hand2 = string.Join("", Convert.ToString(long.Parse(Console.ReadLine()), 2).Reverse());
-            var hand3 = string.Empty;
-            var hand4 = "";
-
-            var a = "A";
-            var a1 = "A";
-            Console.WriteLine("Internate: " + object.ReferenceEquals(a,a1));
-            Console.WriteLine("Compare: " + string.Compare(hand3,hand4) + "| RefEq:  " + object.ReferenceEquals(hand3,hand4)) ;
-            Console.WriteLine($"HAND: {hand}");
-            Console.WriteLine($"HAND2: {hand2}");
-
-            for (var i = 0; i < hand.Length; i++)
+            var a = 5;
+            while (a-- > 0)
             {
-                cards[i] += (hand[i] - '0');
+                Console.WriteLine(a);
             }
+            return "DONE";
         }
-
-        // instead of for loop that counts
-        var hasFullDeck = cards.Count(x => x > 0) == cards.Length;
-
-        Console.WriteLine(hasFullDeck ? "Full deck" : "Wa wa!");
-
-        for (var i = 0; i < cards.Length; i++)
+        finally
         {
-            if (cards[i] % 2 == 0)
-            {
-                var typeIndex = i % types.Length;
-                var suitIndex = i / types.Length;
-
-                Console.Write(types[typeIndex] + "" + suits[suitIndex] + " ");
-            }
+            PrintFinally();
         }
     }
 }
