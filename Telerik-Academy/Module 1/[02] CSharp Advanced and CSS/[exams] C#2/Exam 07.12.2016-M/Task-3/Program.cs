8494b7ffe81c673ad7857a99f8d75ee325ced28f
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-07-24 15:34:33 +0300

    03.-
    50% faster, 9% more memory (the ?un-garbaged? references) - compared to the author's solution

--- Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/Task-3/Program.cs
+++ Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/Task-3/Program.cs
@@ -1,78 +1,14 @@
-﻿using System;
-
-namespace Task_3
+﻿namespace Task_3
 {
+    using System;
     using System.Numerics;
     using System.Text;
-    using System.Text.RegularExpressions;
 
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var pattern = Console.ReadLine();
-            var count = int.Parse(Console.ReadLine());
-            var sb = new StringBuilder();
-            while (count-- > 0)
-            {
-                sb.Append(Console.ReadLine() + " ");
-            }
-            var text = sb.ToString().Trim();
-//            Console.WriteLine(text);
 
-            var patternIndexInString = text.IndexOf(pattern);
-
-            var dotIndex = text.IndexOf(".", patternIndexInString);
-            var questionMarkIndex = text.IndexOf("?", patternIndexInString);
-
-            ////TODO: [#mistake1]Problematic sectio -> punctuation definition
-            var punctuation = (dotIndex < questionMarkIndex && dotIndex >= 0 && questionMarkIndex >= 0)? "." : "?"; // use REGEX
-            Console.WriteLine("Punctuatio: " + punctuation);
-            ////todo: solution : from the pattern to the first occurrence of if(.)else if(?)
-
-            var subStrToCalc = string.Empty;
-            ////Todo: [#mistake2] +1 checking/computing/ of same value (punc)
-            ////Todo: [#mistake3] repetitive computational override of the same variables  ||=> solution: "Answer the question: what is the result?"
-            if (punctuation == ".")
-            {
-                for (int i = patternIndexInString; i >= 0; i--)
-                {
-                    //                    if (text[i] >= 'A' && text[i] <= 'Z')
-                    if (text[i] >= 65 && text[i] <= 90)
-                    {
-                        subStrToCalc = text.Remove(patternIndexInString);
-                        subStrToCalc = subStrToCalc.Substring(i);
-                        break;
-                    }
-                }
-            }
-            else if (punctuation == "?")
-            {
-                subStrToCalc = text.Remove(questionMarkIndex);
-                Console.WriteLine(subStrToCalc);
-                subStrToCalc = subStrToCalc.Substring(patternIndexInString + pattern.Length);
-                Console.WriteLine(subStrToCalc);
-
-                // html from html5
-            }
-
-            subStrToCalc = subStrToCalc.ToUpper();
-//            Console.WriteLine(subStrToCalc);
-            subStrToCalc = subStrToCalc.Replace(" ", string.Empty);////imperfect - use regex for all the types of empty spaces
-//            Console.WriteLine(subStrToCalc);
-
-            BigInteger result = 0;
-            foreach (var ch in subStrToCalc)
-            {
-                result += ch;
-            }
-
-            Console.WriteLine(result);
         }
     }
 }
-/*
-Statement(.) - The substring starts from the beginning of the sentence(inclusive), till the beginning of the keyword(exclusive) we are looking for.
-Question(?) - The substring starts from the end of the keyword(exclusive) we are looking for, till the end of the sentence(without the question mark though).*/
-
-    // result += UpperCase noEmptySpaces
--- Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/Task-4/Program.cs
+++ Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/Task-4/Program.cs
@@ -2,11 +2,54 @@
 
 namespace Task_4
 {
+    using System.Numerics;
+    using System.Text;
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var pattern = Console.ReadLine();
+            var count = int.Parse(Console.ReadLine());
+
+            var sb = new StringBuilder();
+            while (count-- > 0) sb.Append(Console.ReadLine() + " ");
+            var text = sb.ToString().Trim();
+
+            var patternIndexInString = text.IndexOf(pattern);
+            var subStrToCalc = string.Empty;
+
+            for (var i = patternIndexInString; i < text.Length; i++)
+            {
+                if (text[i] == '.')
+                {
+                    subStrToCalc = text.Remove(patternIndexInString);
+                    for (var j = patternIndexInString; j >= 0; j--)
+                    {
+                        if (text[j] >= 'A' && text[j] <= 'Z')
+                        {
+                            subStrToCalc = subStrToCalc.Substring(j);
+                            break;
+                        }
+                    }
+                    break;
+                }
+                else if (text[i] == '?')
+                {
+                    subStrToCalc = text.Remove(i);
+                    subStrToCalc = subStrToCalc.Substring(patternIndexInString + pattern.Length);
+                    break;
+                }
+            }
+            ////Todo: [#mistake3] repetitive computational override of the same variables  ||=> solution: "Answer the question: what is the result?"
+            subStrToCalc = subStrToCalc.ToUpper();
+            subStrToCalc = subStrToCalc.Replace(" ", string.Empty);
+
+            BigInteger result = 0;
+            foreach (var ch in subStrToCalc) result += ch;
+
+            Console.WriteLine(result);
         }
     }
+    //// 50% faster, 9% more memory (the ?un-garbaged? references)
 }
