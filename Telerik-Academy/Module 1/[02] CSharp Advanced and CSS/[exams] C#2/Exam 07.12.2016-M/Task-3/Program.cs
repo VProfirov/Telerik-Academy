8e6917bef6324e043e97658651c14232d03a6497
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-07-25 21:56:55 +0300

    04
    80

--- Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/Task-3/Program.cs
+++ Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/Task-3/Program.cs
@@ -29,47 +29,75 @@
 
 ////            Utility.ConsolePrint(directions,denMap);
 
-            var startPosition = 0;
+            int[] startPosition = { 0, 0 };
             for (int c = 0; c < col; c++)
             {
-                if (denMap[0, c] == "s") startPosition = c;
+                if (denMap[0, c] == "s") startPosition[1] = c;
             }
 
-            ////Conditions - TEMP
-            var sneaky = 3;
-            var i = 0;
-            var moveCount = i++;
-            if (moveCount % 5 == 0)
+            var message = string.Empty;
+            var snakeLength = 3;
+            var moveCount = 0;
+            var snakeCurrentPosition = startPosition;
+            var shouldContinue = true;
+            do
             {
-                sneaky--;
+                snakeCurrentPosition = ExecuteMove(snakeCurrentPosition, directions[moveCount]);
+
+                if ((moveCount + 1) % 5 == 0) 
+                {
+                    snakeLength--;
+                }
+
+                if (snakeLength <= 0)
+                {
+                    message = string.Format("Snacky will starve at [{0},{1}]",snakeCurrentPosition[0],snakeCurrentPosition[1]);
+                    shouldContinue = false;
+                    break;
+                }
+                if (snakeCurrentPosition[0] >= denMap.GetLength(0))
+                {
+                    message = string.Format("Snacky will be lost into the depths with length {0}", snakeLength);
+                    shouldContinue = false;
+                    break;
+                }
+
+                // den & den-morphing
+                switch (denMap[snakeCurrentPosition[0],snakeCurrentPosition[1]]) // out of range - 5
+                {
+                    case ".": break;
+                    case "*": snakeLength++;
+                        denMap[snakeCurrentPosition[0], snakeCurrentPosition[1]] = ".";
+                        break;
+                    case "#": message = string.Format("Snacky will hit a rock at [{0},{1}]",snakeCurrentPosition[0],snakeCurrentPosition[1]);
+                        shouldContinue = false;
+                        break;
+                    case "s": message = string.Format("Snacky will get out with length {0}",snakeLength);
+                        shouldContinue = false;
+                        break;
+
+                }
             }
-            if (denMap[row, col] == "#")
+            while (directions.Length > ++moveCount && shouldContinue); // movecount out of bound
+
+            if (string.IsNullOrEmpty(message))
             {
-                //// dead hit a wall - result message
-                //break;
+                message = string.Format("Snacky will be stuck in the den at [0,1]", snakeCurrentPosition[0], snakeCurrentPosition[1]);
             }
-            if (denMap[row,col] == "*")
+
+            Console.WriteLine(message);
+        }
+
+        private static int[] ExecuteMove(int[] position,string direction)
+        {
+            switch (direction)
             {
-                sneaky++;
-                denMap[row, col] = "."; //empty space
+                case "d": position[0]++; break;
+                case "u": position[0]--; break;
+                case "r": position[1]++; break;
+                case "l": position[1]--; break;
             }
-            if (sneaky == 0)
-            {
-                //// dead (starved) - result message
-                //break;
-            }
-            var sneakyPositionRow = 0;
-            var sneakyPositionCol = 0; // sneakyPositionCol = sneakyPositionCol++ % Col
-            if (sneakyPositionRow > row)
-            {
-                //// lost in depth - result message
-                //break;
-            }
-            if (moveCount == directions.Length)
-            {
-                //// lost in den = result message
-                /// break
-            }
+            return position;
         }
     }
 }
--- Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/TestLand/Program.cs
+++ Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-M/TestLand/Program.cs
@@ -3,30 +3,115 @@ using System.Linq;
 
 class Solution
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine(PrintN());
-    }
+    // Better use constants, rather than strings in the code. Easyer to modify if needed
+    const string GET_OUT_MSG = "Snacky will get out with length {0}";
+    const string BUMP_IN_ROCK_MSG = "Snacky will hit a rock at [{1},{2}]";
+    const string LOST_IN_DEPTHS_MSG = "Snacky will be lost into the depths with length {0}";
+    const string STARVE_MSG = "Snacky will starve at [{1},{2}]";
+    const string STILL_IN_DEN_MSG = "Snacky will be stuck in the den at [{1},{2}]";
 
-    public static void PrintFinally()
-    {
-        Console.WriteLine("Finally");
-    }
+    const int looseWeightMoveCount = 5;
 
-    public static string PrintN()
+    // Use static fields in order to not pass all these parameters in all methods
+    static char[,] field;
+    static char[] moves;
+    static int[] pos;
+    static int[] dir = { 1, 0 };
+    static int snakeLength = 3;
+
+    static void Main()
     {
-        try
+        // read input
+        int[] size = Console.ReadLine().Split('x').Select(int.Parse).ToArray();
+        field = ReadField(size[0], size[1]);
+        moves = Console.ReadLine().Split(',').Select(x => x[0]).ToArray();
+
+
+        string message = STILL_IN_DEN_MSG;
+        bool shouldContinue = true;
+        for (int i = 0; i < moves.Length && shouldContinue; i++)
         {
-            var a = 5;
-            while (a-- > 0)
+            ExecuteMove(moves[i]);
+            // evaluat Snacky's length
+            snakeLength -= (i != 0 && (i + 1) % looseWeightMoveCount == 0) ? 1 : 0;
+
+            if (snakeLength <= 0)
             {
-                Console.WriteLine(a);
+                message = STARVE_MSG;
+                shouldContinue = false;
+                break;
             }
-            return "DONE";
+
+            if (pos[0] >= field.GetLength(0))
+            {
+                message = LOST_IN_DEPTHS_MSG;
+                shouldContinue = false;
+                break;
+            }
+
+            CheckForRightLeftLoop();
+            switch (field[pos[0], pos[1]])
+            {
+                case '.': break;
+                case '*':
+                    snakeLength++;
+                    field[pos[0], pos[1]] = '.';
+                    break;
+                case '#':
+                    message = BUMP_IN_ROCK_MSG;
+                    shouldContinue = false;
+                    break;
+                case 's':
+                    message = GET_OUT_MSG;
+                    shouldContinue = false;
+                    break;
+            }
         }
-        finally
+
+        Console.WriteLine(message, snakeLength, pos[0], pos[1]);
+    }
+
+    private static char[,] ReadField(int rows, int cols)
+    {
+        char[,] field = new char[rows, cols];
+
+        string line;
+        for (int row = 0; row < rows; row++)
         {
-            PrintFinally();
+            line = Console.ReadLine();
+            for (int col = 0; col < cols; col++)
+            {
+                field[row, col] = line[col];
+                if (line[col] == 's')
+                {
+                    pos = new int[] { row, col };
+                }
+            }
+        }
+
+        return field;
+    }
+
+    private static void ExecuteMove(char move)
+    {
+        switch (move)
+        {
+            case 'd': pos[0]++; break;
+            case 'u': pos[0]--; break;
+            case 'r': pos[1]++; break;
+            case 'l': pos[1]--; break;
+        }
+    }
+
+    private static void CheckForRightLeftLoop()
+    {
+        if (pos[1] < 0)
+        {
+            pos[1] = field.GetLength(1) - 1;
+        }
+        else if (pos[1] >= field.GetLength(1))
+        {
+            pos[1] = 0;
         }
     }
 }
