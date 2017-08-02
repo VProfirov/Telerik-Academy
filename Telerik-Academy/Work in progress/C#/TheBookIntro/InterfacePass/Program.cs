ea56dc8bd296459d40c2bb9bca22ae3779fc10e2
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-08-02 11:46:58 +0300

    00

--- Telerik-Academy/Work in progress/C#/TheBookIntro/InterfacePass/Program.cs
+++ Telerik-Academy/Work in progress/C#/TheBookIntro/InterfacePass/Program.cs
@@ -1,74 +1,83 @@
-﻿using System;
-
-namespace InterfacePass
+﻿namespace InterfacePass
 {
-    class Factory
+    using System;
+
+    internal class Factory
     {
-        static void Main(string[] args)
+        private string someField;
+
+        public Factory()
+        {
+        }
+        
+        private interface ILectureResource
+        {
+            string Url { get; set; }
+
+            string Username { get; set; }
+        }
+
+        public string Type { get; set; }
+
+        // TODO: shiiiii
+        private static void Main(string[] args)
         {
             var factory = new Factory();
+
             var rV = factory.FactoryResource("video", "VidRes", "vr:url", DateTime.Today);
             var rD = factory.FactoryResource("demo", "demo-res", "some demo:url", DateTime.Today.AddDays(2));
 
-//            rV.
+            // rV.
         }
 
-        ILectureResource FactoryResource(string type, string username, string url, DateTime date)
+        private ILectureResource FactoryResource(string type, string username, string url, DateTime date)
         {
             switch (type)
             {
-                case "video":
-                    return new VideoResource(username, url, date);
-                case "demo":
-                    return new DemoResource(username, url);
+                case "video": return new VideoResource(username, url, date);
+                case "demo": return new DemoResource(username, url);
                 default: throw new ArgumentException();
             }
-            //            ILectureResource lr;
-            //            switch (type)
-            //            {
-            //                case "video": lr = new VideoResource(username, url, date);
-            //                    break;
-            //                case "demo": lr = new DemoResource(username,url);
-            //                    break;
-            //                default: throw new ArgumentException();
-            //            }
-            //
-            //            return lr;
+
+            // ILectureResource lr;
+            // switch (type)
+            // {
+            // case "video": lr = new VideoResource(username, url, date);
+            // break;
+            // case "demo": lr = new DemoResource(username,url);
+            // break;
+            // default: throw new ArgumentException();
+            // }
+            // return lr;
         }
 
-        interface ILectureResource
-        {
-            string Username { get; set; }
-
-            string Url { get; set; }
-        }
-
-        class VideoResource : ILectureResource
-        {
-            public VideoResource(string username, string url, DateTime date)
-            {
-                Username = username;
-                Url = url;
-                StartingDate = date;
-            }
-            public string Username { get; set; }
-
-            public string Url { get; set; }
-
-            public DateTime StartingDate { get; }
-
-        }
-
-        class DemoResource : ILectureResource
+        private class DemoResource : ILectureResource
         {
             public DemoResource(string username, string url)
             {
-                Username = username;
-                Url = url;
+                this.Username = username;
+                this.Url = url;
             }
-            public string Username { get; set; }
 
             public string Url { get; set; }
+
+            public string Username { get; set; }
+        }
+
+        private class VideoResource : ILectureResource
+        {
+            public VideoResource(string username, string url, DateTime date)
+            {
+                this.Username = username;
+                this.Url = url;
+                this.StartingDate = date;
+            }
+
+            public DateTime StartingDate { get; }
+
+            public string Url { get; set; }
+
+            public string Username { get; set; }
         }
     }
 }
