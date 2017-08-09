8c5a1c0db88626b76689504802d8c12913bb7455
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-08-09 21:51:37 +0300

    quick js error proc

--- Telerik-Academy/Module 1/[03] CSharp OOP and JavaScript Fundamentals/JS Fundamentals/someScript.js
+++ Telerik-Academy/Module 1/[03] CSharp OOP and JavaScript Fundamentals/JS Fundamentals/someScript.js
@@ -1,1 +1,35 @@
-console.log("hi");
+function solve() {
+    const validation = {
+        ValidateString(str) {
+            if (typeof(str) != "string") {
+                throw Error("Invalid string");
+            }
+            return str;
+        }
+    };
+
+    class Product {
+        constructor(manufacturer, type, price) {
+            this._manufacturer = manufacturer;
+            this._type = type;
+            this._price = price;
+        }
+    }
+
+    class CellPhone extends Product() {
+        constructor(charger, ...params) {
+            super(...params);
+            this._charger = ValidateString(charger);
+        }
+        set(charger) { this._charger = charger; }
+        get() { return this._charger; }
+    }
+
+    return {
+        cellphone: new CellPhone(charger, manufacturer, type, price)
+    }
+}
+
+let solution = solve();
+let cellphone1 = solution.cellphone("some Charger", "Manufacturer", "type", "100$");
+console.log(cellphone1);
