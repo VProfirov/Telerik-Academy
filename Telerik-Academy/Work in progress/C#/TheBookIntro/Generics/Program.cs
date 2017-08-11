d9f48c7ff6d9d2f6a165c054cd020c81b4adffcc
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-08-11 22:00:00 +0300

    grrr - kraken and path-length

---
+++ Telerik-Academy/Work in progress/C#/TheBookIntro/Generics/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal<Cat> dog = new Animal<Cat>();
+            Console.WriteLine(dog.Name);
+        }
+
+        public class AnimalShelter<T>
+        {
+            public IList<T> AnimaList { get; set; } = new List<T>();
+        }
+
+        public class Animal<T>
+        {
+            public Animal():this("Sharo",0)
+            {
+            }
+            public Animal(string name, int age)
+            {
+                this.Name = name;
+                this.Age = age;
+            }
+
+            public int Age { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        public class Dog
+        {
+            public override string ToString()
+            {
+                return $"Bou";
+            }
+        }
+
+        public class Cat
+        {
+            public override string ToString()
+            {
+                return $"Mauuu";
+            }
+        }
+    }
+}
