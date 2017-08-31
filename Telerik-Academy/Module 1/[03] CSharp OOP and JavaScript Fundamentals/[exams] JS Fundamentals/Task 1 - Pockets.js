6b1ef422ff30f541cb40f97c94256c5e8e5bf36f
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-09-01 00:23:03 +0300

    01

--- Telerik-Academy/Module 1/[03] CSharp OOP and JavaScript Fundamentals/[exams] JS Fundamentals/Task 1 - Pockets.js
+++ Telerik-Academy/Module 1/[03] CSharp OOP and JavaScript Fundamentals/[exams] JS Fundamentals/Task 1 - Pockets.js
@@ -2,10 +2,8 @@ function solve(args) {
     let nodes = args[0].split(" ").map(Number);
 
     let sum = 0;
-    let pocket = 0;
-    for (var index = 1; index < nodes.length - 1; index++) {
-
-        if (isPeak(index, nodes)) {
+    for (let index = 2; index < nodes.length - 2; index++) {
+        if (isPocket(index, nodes)) {
             sum += nodes[index];
         }
     }
@@ -13,20 +11,20 @@ function solve(args) {
     // return sum;
 
     function isPocket(index, nodes) {
-        return isPeak(index - 1, nodes) && isValey(index, nodes) && isPeak(index + 1, nodes);
+        return isPeak(index - 1, nodes) && isValley(index, nodes) && isPeak(index + 1, nodes);
     }
 
-    function isValey(index, nodes) {
+    function isValley(index, nodes) {
         return nodes[index] < nodes[index - 1] && nodes[index] < nodes[index + 1];
     }
 
     function isPeak(index, nodes) {
-
+        return nodes[index] > nodes[index - 1] && nodes[index] > nodes[index + 1];
     }
 }
 
 
 
 // solve(["53 20 1 30 2 40 3 10 1"]);
-// solve(["53 20 1 30 30 2 40 3 10 1"]);
-solve(["53 20 1 30 2 40 3 3 10 1"]);
+solve(["53 20 1 30 30 2 40 3 10 1"]);
+// solve(["53 20 1 30 2 40 3 3 10 1"]);
