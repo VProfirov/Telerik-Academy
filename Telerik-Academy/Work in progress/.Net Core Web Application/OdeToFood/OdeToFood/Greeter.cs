67124a85b076f867f99df3730ba011e1cfaeb9e0
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-09-03 22:27:06 +0300

    OdeToFood - Allen ... hmmm

---
+++ Telerik-Academy/Work in progress/.Net Core Web Application/OdeToFood/OdeToFood/Greeter.cs
@@ -0,0 +1,15 @@
+﻿namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+
+    public class Greeter:IGreeter
+    {
+        public string GetGreeting()
+        {
+            return "Hellof from the Greeter!";
+        }
+    }
+}
--- Telerik-Academy/Work in progress/.Net Core Web Application/OdeToFood/OdeToFood/Startup.cs
+++ Telerik-Academy/Work in progress/.Net Core Web Application/OdeToFood/OdeToFood/Startup.cs
@@ -5,20 +5,38 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace OdeToFood
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +45,9 @@ namespace OdeToFood
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello00000     World!");
+//                var message = Configuration["Greeting"];
+                var message = greeter.GetGreeting();
+                await context.Response.WriteAsync(message);
             });
         }
     }
