635d8e7afbe3d0922566f9951a98e26e2514c068
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-09-04 15:51:36 +0300

    dev - Builder Pattern

---
+++ Telerik-Academy/Work in progress/Design patterns v0.2 - Core/Design Patterns/Builder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Sandwich(BreadType.dark, false, CheeseType.french, MeatType.chicken, false, false,
+                new List<string> {"Tomato"}).Display();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Telerik-Academy/Work in progress/Design patterns v0.2 - Core/Design Patterns/Builder/Sandwiches.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Builder
+{
+    public class Sandwiches
+    {
+        public BreadType BreadType { get; set; }
+        public CheeseType CheeseType { get; set; }
+        public MeatType MeatType { get; set; }
+        public bool IsToasted { get; set; }
+        public bool hasMustard { get; set; }
+        public bool hasMayo { get; set; }
+        public List<string> Vegetables;
+
+        public Sandwiches()
+        {
+            
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Sandwitch on {this.BreadType}");
+            if (IsToasted)
+            {
+                Console.WriteLine("Toasted");
+            }
+            if (hasMayo)
+            {
+                Console.WriteLine("With Mayo");
+            }
+            if (hasMustard)
+            {
+                Console.WriteLine("With Mustard");
+            }
+            Console.WriteLine($"Meat: {this.MeatType}");
+            Console.WriteLine($"Cheese: {this.CheeseType}");
+            Console.WriteLine("Vegetables: ");
+            foreach (var vegetable in Vegetables)
+            {
+                Console.WriteLine($" {vegetable}");
+            }
+        }
+    }
+
+    public enum CheeseType
+    {
+        normal,
+        french,
+        bulgarian
+    }
+
+    public enum BreadType
+    {
+        dark,
+        white,
+        seeds
+    }
+
+    public enum MeatType
+    {
+        chicken,
+        beef,
+        fish
+    }
+}
