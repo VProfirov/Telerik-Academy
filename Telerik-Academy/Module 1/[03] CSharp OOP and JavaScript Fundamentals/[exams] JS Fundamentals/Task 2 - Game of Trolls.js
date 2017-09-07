f5f27d3efe72e64499345e43d29a347682b61140
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-09-07 19:50:15 +0300

    70

--- Telerik-Academy/Module 1/[03] CSharp OOP and JavaScript Fundamentals/[exams] JS Fundamentals/Task 2 - Game of Trolls.js
+++ Telerik-Academy/Module 1/[03] CSharp OOP and JavaScript Fundamentals/[exams] JS Fundamentals/Task 2 - Game of Trolls.js
@@ -9,9 +9,9 @@ function solve(args) {
     let dimensionsOfBoard = args[0].split(" ").map(Number);
     let startsOfUnits = args[1].split(";").map(x => x.split(" ").map(Number));
     let units = {
-        'Wboup': { r: startsOfUnits[0][0], c: startsOfUnits[0][1] },
-        'Nbslbub': { r: startsOfUnits[1][0], c: startsOfUnits[1][1] },
-        'Lsjtujzbo': { r: startsOfUnits[2][0], c: startsOfUnits[2][1] }
+        Wboup: { r: startsOfUnits[0][0], c: startsOfUnits[0][1], trapped: false },
+        Nbslbub: { r: startsOfUnits[1][0], c: startsOfUnits[1][1], trapped: false },
+        Lsjtujzbo: { r: startsOfUnits[2][0], c: startsOfUnits[2][1] }
     }
 
     let Wboup = units.Wboup;
@@ -32,4 +32,95 @@ function solve(args) {
         board.push([]);
     }
 
+    function isInside(r, c) {
+        return (0 <= r) && (r < dimensionsOfBoard[0]) && (0 <= c) && (c < dimensionsOfBoard[1]);
+    }
+
+    //impure functions func(_,_..)
+    function caught() {
+        //Math.abs(Wboup.r - Princess.r) <= 1 
+        // the distance between Wboup & Princess is 1 or less on the row(r) axis(y)
+        let byWboup = (Math.abs(Wboup.r - Princess.r) <= 1) && (Math.abs(Wboup.c - Princess.c) <= 1);
+        let byNbslbub = (Math.abs(Nbslbub.r - Princess.r) <= 1) && (Math.abs(Nbslbub.c - Princess.c) <= 1);
+        return byWboup || byNbslbub;
+    }
+
+    function escaped() {
+        return (Wboup.trapped && Nbslbub.trapped) ||
+            //The coordinates(r/c) of the princess MATCH the coordinates of the exit
+            ((Princess.r === (dimensionsOfBoard[0] - 1)) && (Princess.c === (dimensionsOfBoard[1] - 1)));
+        //last row last call
+    }
+
+    function onTheSameCell() {
+        return (Wboup.r && Nbslbub.r) && (Wboup.c && Nbslbub.c);
+    }
+
+    //cut off everything after 2 -included
+    let commands = args.slice(2);
+    for (var cmd of commands) {
+        let cmdSplit = cmd.split(' ');
+
+        if (cmdSplit.first() == 'mv') {
+            let direction = directions[cmdSplit.last()]; //[cmdSplit[2]]
+            let unit = units[cmdSplit[1]];
+
+            //what allows a unit to move => not trapped and inside the board
+            let nextStepR = unit.r + direction.r;
+            let nextStepC = unit.c + direction.c;
+            if (!unit.trapped && isInside(nextStepR, nextStepC)) {
+                unit.r = nextStepR;
+                unit.c = nextStepC;
+
+                //check for trap on that location
+                let trap = board[unit.r][unit.c];
+                if (trap) {
+                    //assign trapped => if there is Wboup or Nbslbub already there (a.k.a trapped)
+                    if (onTheSameCell()) {
+                        //free
+                        Wboup.trapped = false;
+                        Nbslbub.trapped = false;
+                        board[unit.r][unit.c] = false;
+                    } else if (unit != Princess) { //not the Princess <->ref(unit) doesn't point to the same obj (ref(Princess)points to)
+                        //trap
+                        unit.trapped = true;
+                    }
+                }
+            }
+
+        } else if (cmd === 'lay trap') {
+            board[Princess.r][Princess.c] = true;
+        } else {
+            throw Error('Command related Problem');
+        }
+    }
+    //Output
+
+    if (caught()) {
+        console.log(`The trolls caught Lsjtujzbo at ${Princess.r} ${Princess.c}`);
+        return; // return > break => return ends the function VS break ends the loop (if(!loop) ends func)
+    } else if (escaped()) {
+        console.log(`Lsjtujzbo is saved! ${Wboup.r} ${Wboup.c} ${Nbslbub.r} ${Nbslbub.c}`);
+        return;
+    } else {
+        throw Error('Problem with the Output');
+    }
 }
+
+let input = [
+    '5 5',
+    '1 1;0 1;2 3',
+    'mv Lsjtujzbo d',
+    'lay trap',
+    'mv Lsjtujzbo d',
+    'mv Wboup r',
+    'mv Wboup r',
+    'mv Nbslbub d',
+    'mv Nbslbub d',
+    'mv Nbslbub d',
+    'mv Nbslbub d',
+    'mv Nbslbub d',
+    'mv Wboup d',
+    'mv Wboup d'
+];
+solve(input);
