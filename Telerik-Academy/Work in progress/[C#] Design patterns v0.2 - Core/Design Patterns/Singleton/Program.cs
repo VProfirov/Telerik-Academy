a1d0cddddeb30959e9441848d3edc6f417818e1a
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-09-26 00:21:54 +0300

    Lazy<Singleton> pattern

---
+++ Telerik-Academy/Work in progress/[C#] Design patterns v0.2 - Core/Design Patterns/Singleton/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Singleton
+    {
+        private static Singleton _instance;
+        private Singleton()
+        {
+            
+        }
+
+        //Not Thread Safe - don't use for web
+        public static Singleton Instance => _instance ?? (_instance = new Singleton());
+    }
+
+    public class LazySingleton
+    {
+        private LazySingleton()
+        {
+        }
+
+        // IS Thread Safe
+        public static LazySingleton Instance => Nested.instance;
+
+        private class Nested
+        {
+            internal static readonly LazySingleton instance = new LazySingleton();
+            static Nested()
+            {
+            }
+        }
+    }
+}
