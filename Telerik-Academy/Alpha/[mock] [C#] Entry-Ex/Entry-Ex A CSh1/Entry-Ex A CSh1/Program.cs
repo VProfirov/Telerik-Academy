bda6d80351c3b049e026f2bf55e242a923aad4a4
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-09-26 13:28:45 +0300

    bgCoder Compile/RunTime  error

--- Telerik-Academy/Alpha/[mock] [C#] Entry-Ex/Entry-Ex A CSh1/Entry-Ex A CSh1/Program.cs
+++ Telerik-Academy/Alpha/[mock] [C#] Entry-Ex/Entry-Ex A CSh1/Entry-Ex A CSh1/Program.cs
@@ -8,16 +8,16 @@ namespace Entry_Ex_A_CSh1
     {
         static void Main(string[] args)
         {
-//            Solution_T1();
+            Solution_T1();
 
-            var input = "asd 100 100 100";
-            var data = input.Split(" ");
-            var command = data[0];
-
-            var tmp = string.Concat(data.TakeLast(data.Length - 1));
-            Console.WriteLine(tmp.Length);
-            Console.WriteLine(data.Length);
-            Console.WriteLine(tmp);
+//            var input = "asd 100 100 100";
+//            var data = input.Split(" ");
+//            var command = data[0];
+//
+//            var tmp = string.Concat(data.TakeLast(data.Length - 1));
+//            Console.WriteLine(tmp.Length);
+//            Console.WriteLine(data.Length);
+//            Console.WriteLine(tmp);
         }
     }
 }
