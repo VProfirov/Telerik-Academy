0fa65e5a61ba51c6c867411789a81d048b1ff2c8
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-09-29 22:36:45 +0300

    Creational Patterns - finish 1/2

---
+++ Telerik-Academy/Work in progress/[C#] Design patterns v0.2 - Core/Design Patterns/Object Pool-antiPatt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Object_Pool_antiPatt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
