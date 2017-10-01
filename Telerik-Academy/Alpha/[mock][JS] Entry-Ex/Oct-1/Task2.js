d7df52a6f56b844c4364d9705dd57b73d0d7bcd0
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-10-01 17:44:04 +0300

    VSC can cast: you as Smurf
    When VSC reconfigures your launch.json after update.
    Don't even get me started on VS

---
+++ Telerik-Academy/Alpha/[mock][JS] Entry-Ex/Oct-1/Task2.js
@@ -0,0 +1,14 @@
+function solve(arg) {
+    let number = Number(arg[0]);
+    let avg = 5;
+    let result = Math.ceil(number / avg);
+    console.log(result);
+}
+solve(["17"]);
+// solve(["17 2 3"]);
+
+// {
+//     console.log(typeof(number) + " is " + number);
+//     let n = arg[0].split(" ").map(Number);
+//     for (let i of n) { console.log(i + " " + typeof(i)); }
+// }
