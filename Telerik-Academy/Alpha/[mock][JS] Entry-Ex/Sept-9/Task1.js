e51bbef582df9922a8d7e5f52be169ca0a24d6e4
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-10-10 18:12:39 +0300

    1st task JS Non Overflowing Number: counter of 10 impl

--- Telerik-Academy/Alpha/[mock][JS] Entry-Ex/Sept-9/Task1.js
+++ Telerik-Academy/Alpha/[mock][JS] Entry-Ex/Sept-9/Task1.js
@@ -1,11 +1,54 @@
 function solve(args) {
     let number = Number(args[0]);
+    let numberOverflowCheck = number < Number.MAX_SAFE_INTEGER - 11;
+
+    let numberAsString = args[0];
+    numberAsString = [...numberAsString].reverse();
+    let result = [];
+    let digit = 0;
+    let remainderX10 = 0;
     let counter = 0;
-    while (counter++ < 10) {
-        console.log(number + counter);
+
+    if (numberOverflowCheck) {
+        while (counter++ < 10) {
+            console.log(number + counter);
+        }
+    } else {
+        while (counter++ < 10) {
+            digit = Number(numberAsString[0]);
+            remainderX10 = 0;
+            result = [];
+
+            digit += counter;
+            if (digit > 9) {
+                result.push(digit % 10);
+                remainderX10 = 1;
+                for (let i = 1; i < numberAsString.length; i++) {
+                    digit = Number(numberAsString[i]) + remainderX10;
+                    if (digit > 9) {
+                        result.push(digit % 10);
+                        remainderX10 = 1;
+                    } else {
+                        result.push(digit);
+                        remainderX10 = 0;
+                    }
+                }
+                if (remainderX10) {
+                    result.push(1);
+                }
+            } else {
+                result.push(digit);
+                for (let i = 1; i < numberAsString.length; i++) {
+                    result.push(Number(numberAsString[i]));
+                }
+            }
+            console.log(result.reverse().join(""));
+        }
     }
 }
 
 solve([
-    "90"
+    // "9007199254749992"
+    "9999999999999992"
+    // "1"
 ])
---
+++ Telerik-Academy/Alpha/[mock][JS] Entry-Ex/testing.js
@@ -0,0 +1,5 @@
+for (var i = 0; counter < 0 && i < 10; i++) {
+    let counter = 0;
+    console.log(i);
+    counter++;
+}
