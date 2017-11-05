e0f9a054a2ef01e1dbe8cc909ebdf514113d9e80
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-11-05 21:32:07 +0200

    00

--- Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-E/Task-2/Program.cs
+++ Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-E/Task-2/Program.cs
@@ -6,7 +6,22 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* TODO: CONDITIONS: 
+             * - **N** will always be in the range of [2..100000]
+             * - in 70 % of the tests **N * *will be below **50 * */
+            var inputCounter = int.Parse(Console.ReadLine());
+            var handsOfCards = new long[inputCounter];
+
+            // TODO: (bit-read <-> 1 is set)
+            for (int i = 0; i < handsOfCards.Length; i++)
+            {
+                handsOfCards[i] = long.Parse(Console.ReadLine());
+            }
+
+            /* TODO: 
+             * LINE 1 => (52 cards hand possible)? "Full deck"   : "Wa wa!"
+             * LINE 2 => (cardCount !% 2) ?=> ascendingOrder..join(" ")
+             * */
         }
     }
 }
