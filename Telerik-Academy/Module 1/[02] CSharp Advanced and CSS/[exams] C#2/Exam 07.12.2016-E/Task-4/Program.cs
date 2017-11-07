b6c822d6ddfaca8d55da217c3197cb15f2b49d98
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-11-07 14:16:21 +0200

    00

--- Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-E/Task-4/Program.cs
+++ Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-E/Task-4/Program.cs
@@ -2,11 +2,21 @@
 
 namespace Task_4
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            long exB1 = 0b1111111111111111111111111100000000000000000000000000;
+            long exN1 = 4503599560261632;
+            string exS1 = Convert.ToString(exB1, 2);
+            long exB2 = 0b0000000000000000000000000011111111111111111111111111;
+            long exN2 = 67108863;
+            string exS2 = Convert.ToString(exB2, 2);
+
+            Console.WriteLine($"{exB1} vs {exN1} == {exB1 == exN1}");
+            Console.WriteLine($"{exB2} vs {exN2} == {exB2 == exN2}");
+
+            var res = exN1 + exN2;
         }
     }
 }
