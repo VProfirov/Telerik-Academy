74a0f4383bdd595bcf1f4366323529b9e9641ad7
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-11-07 20:52:00 +0200

    exam-Task2 C#2 + Logic Step Algo

--- Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-E/Task-2/Program.cs
+++ Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-E/Task-2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace Task_2
 {
@@ -12,21 +14,51 @@ namespace Task_2
             var inputCounter = int.Parse(Console.ReadLine());
             var handsOfCards = new long[inputCounter];
 
-            // TODO: (bit-read <-> 1 is set)
             for (int i = 0; i < handsOfCards.Length; i++)
             {
                 handsOfCards[i] = long.Parse(Console.ReadLine());
             }
-
-            /* TODO: 
-             * LINE 1 => (52 cards hand possible)? "Full deck"   : "Wa wa!"
-             * LINE 2 => (cardCount !% 2) ?=> ascendingOrder..join(" ")
-             * */
-
+            
             /* TODO: STATES
              * 4 type of cards -> c d h s
              * a type is of 0-12 cards -> 2-10-J-A
              */
+            var cardTypes = "23456789TJQKA";
+            var suitTypes = "cdhs";
+            var cardsWeHave = new int[52];
+
+            // TODO: COMPUTE: (bit-read <-> 1 -> has)
+            long mask = 1;
+            for (int hand = 0; hand < handsOfCards.Length; hand++)
+            {
+                for (int cardIndex = 0; cardIndex < 52; cardIndex++)
+                {
+                    int check = (int)((handsOfCards[hand] & (mask << cardIndex)) >> cardIndex);
+                    cardsWeHave[cardIndex] += check;
+                }
+            }
+
+            /* TODO: OUTPUTS
+             * LINE 1 => (52 cards hand possible)? "Full deck"   : "Wa wa!"
+             * LINE 2 => (cardCount !% 2) ?=> ascendingOrder..join(" ")
+             * */
+            //LINE 1
+            var line1 = (cardsWeHave.Count(x => x > 0) == cardsWeHave.Length ? "Full deck" : "Wa wa!");
+            Console.WriteLine(line1);
+
+            //LINE 2
+            var sbLine2 = new StringBuilder();
+            for (int i = 0; i < cardsWeHave.Length; i++)
+            {
+                if (cardsWeHave[i] % 2 != 0)
+                {
+                    var typeIndex = i % cardTypes.Length;  // index inside a cycle
+                    var suitsIndex = i / cardTypes.Length; // integer cycle (integer devision)
+                    sbLine2.Append(cardTypes[typeIndex] + "" + suitTypes[suitsIndex] + " ");
+                }
+            }
+            var line2 = sbLine2.ToString();
+            Console.WriteLine(line2);
         }
     }
 }
--- Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-E/Task-4/Program.cs
+++ Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-E/Task-4/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
 
 namespace Task_4
 {
@@ -8,15 +11,51 @@ namespace Task_4
         {
             long exB1 = 0b1111111111111111111111111100000000000000000000000000;
             long exN1 = 4503599560261632;
-            string exS1 = Convert.ToString(exB1, 2);
+//            string exS1 = Convert.ToString(exB1, 2);
             long exB2 = 0b0000000000000000000000000011111111111111111111111111;
             long exN2 = 67108863;
-            string exS2 = Convert.ToString(exB2, 2);
+//            string exS2 = Convert.ToString(exB2, 2);
+            long exN3 = 1;
 
-            Console.WriteLine($"{exB1} vs {exN1} == {exB1 == exN1}");
-            Console.WriteLine($"{exB2} vs {exN2} == {exB2 == exN2}");
+            var dataSet = new List<long>(){exN1,exN2,exN3};
 
-            var res = exN1 + exN2;
+//            Console.WriteLine($"{exB1} vs {exN1} == {exB1 == exN1}");
+//            Console.WriteLine($"{exB2} vs {exN2} == {exB2 == exN2}");
+
+//            Console.WriteLine(Convert.ToString(long.MinValue,2));
+
+            void Tracker()
+            {
+                long target = exB1;
+                long mask = 1;
+                for (int i = 0; i < 52; i++)
+                {
+                    var start = Convert.ToString(target, 2);
+                    Console.WriteLine($"start : {start}");
+                    var cutter = start.Remove(start.Length - 1 - i);
+                    Console.WriteLine($"cutter: {cutter}");
+
+                    long result = (target & (mask << i)) >> i;
+                    Console.WriteLine($"result: {result} at position {i}");
+                    // breaks for each BEYOND the border of the number :: border <-> added 0s
+                }
+            }
+//            Tracker();
+
+            long mask = 1;
+            var cardsWeHave = new int[52];
+            var cards = "23456789TJQKA";
+            var suits = "cdhs";
+            for (int hand = 0; hand < dataSet.Count; hand++)
+            {
+                for (int cardIndex = 0; cardIndex < 52; cardIndex++)
+                {
+                    int check = (int)(dataSet[hand] & (mask << cardIndex) >> cardIndex);
+                    cardsWeHave[cardIndex] += check;
+                }
+            }
+
+
         }
     }
 }
