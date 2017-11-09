c95b620aba2352c5e36f30b24fea88b4696669ee
Vasil Profirov <Vasil.Profirov@gmail.com>
2017-11-09 22:41:57 +0200

    00

--- Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-E/Task-3/Program.cs
+++ Telerik-Academy/Module 1/[02] CSharp Advanced and CSS/[exams] C#2/Exam 07.12.2016-E/Task-3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Task_3
 {
@@ -6,11 +7,39 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
+            var dimensions = Console.ReadLine().Split('x').Select(int.Parse) as int[];
+            var matrix = new char[dimensions[0]][];
+
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                matrix[i] = Console.ReadLine().ToCharArray();
+            }
+            var directions = Console.ReadLine().Split(',').Aggregate((previous,current) => previous + current).ToCharArray();
+            var move = string.Empty;
+            foreach (var direction in directions)
+            {
+                switch (direction)
+                {
+                    case 's': move = "down"; break;
+                    case 'w': move = "up"; break;
+                    case 'a': move = "left"; break;
+                    case 'd': move = "right"; break;
+                    default: /*direct move of the stepper in the matrix*/break;
+                }
+            }
+
             char step = '\u0000';
-            Console.WriteLine(step);
             switch (step)
             {
+                case '-':
+                   break;
+                case '%':
+                case '@':
+                case 'e':
+                default:
+                  break;
             }
+            
         }
     }
 }
