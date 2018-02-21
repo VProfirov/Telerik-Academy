4da759935f32a72e84de392bf42fb23f5786a404
Vasil Profirov <Vasil.Profirov@gmail.com>
2018-02-22 00:40:30 +0200

    0

--- Alpha/Entry-Ex-300x2-N/JS/Crooked Stairs.js
+++ Alpha/Entry-Ex-300x2-N/JS/Crooked Stairs.js
@@ -1,5 +1,17 @@
-function solve() {
+function solve(args) {
+    let l1 = [];
+    let l2 = [];
+    l1[0] = args[0][0];
+    l2[0] = args[0][1];
+    l2[1] = args[0][2];
+    let numberOfLayers = args[0][3];
 
+    let layers = [args[0][0],
+        [args[0][1], args[0][2]]
+    ];
+    console.log(layers);
+    layers.length = 5;
+    console.log(layers.length);
 }
 
 solve(
