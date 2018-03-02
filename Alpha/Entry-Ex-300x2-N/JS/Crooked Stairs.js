51d2400885f193a02e84b33ee4c87c90a148da3d
Vasil Profirov <Vasil.Profirov@gmail.com>
2018-03-02 18:28:52 +0200

    JS to Judge Crooked ExamAlpa

--- Alpha/Entry-Ex-300x2-N/JS/Crooked Stairs.js
+++ Alpha/Entry-Ex-300x2-N/JS/Crooked Stairs.js
@@ -1,10 +1,33 @@
-function solve(params) {
+const getGets = (arr) => {
+    let index = 0;
+
+    return () => {
+        const toReturn = arr[index];
+        index += 1;
+        return toReturn;
+    };
+};
+const test = [
+    1, 2, 7, 11
+    //2, 1, 5, 3
+    // 3, -2, -1, 4
+];
+const gets = this.gets || getGets(test);
+const print = this.print || console.log;
+solve(gets);
+
+function solve(getParams) {
+    let params = [];
+    for (let i = 0; i < 4; i++) {
+        params.push(Number(getParams()));
+    }
+
     let bricks = [params[0], params[1], params[2]];
     let layers = params[3];
     bricks.length = TrNSq(layers);
     bricksFiller(bricks);
     // console.log(bricks);
-    printer(bricks, layers);
+    builder(bricks, layers);
 
     function bricksFiller(bricks) {
         for (let i = 3; i < bricks.length; i++) {
@@ -17,23 +40,17 @@ function solve(params) {
         return result;
     }
 
-    function printer(bricks, layers) {
+    function builder(bricks, layers) {
         let position = 0;
         let sizeOfStep = 1;
         for (let layer = 0; layer < layers; layer++) {
-            let print = "";
+            let buildingFloor = "";
             for (let i = position; i < position + sizeOfStep; i++) {
-                print += bricks[i] + " ";
+                buildingFloor += bricks[i] + " ";
             }
-            console.log(print.trim());
-
+            print(buildingFloor.trim());
             position += sizeOfStep;
             sizeOfStep++;
         }
     }
 }
-
-solve(
-    // [2, 1, 5, 3]
-    [3, -2, -1, 4]
-);
