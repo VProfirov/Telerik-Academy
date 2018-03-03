58065b1a2d24b324c1b8df72eb9c5d8bc507d3d0
Vasil Profirov <Vasil.Profirov@gmail.com>
2018-03-03 23:58:09 +0200

    crooked stairs js fail

--- Alpha/Entry-Ex-300x2-N/JS/Crooked Stairs v02[LayeredDS].js
+++ Alpha/Entry-Ex-300x2-N/JS/Crooked Stairs v02[LayeredDS].js
@@ -1,26 +1,62 @@
-//NOTE:asd TODO: asd FIXME: asd DEBUG: asd HACK: asd REVIEW:css
-function solve(args) {
-    let bricks = [];
-    bricks[0] = [args[0]];
-    bricks[1] = [args[1], args[2]];
-    let layersCount = args[3];
+const getGets = (arr) => {
+    let index = 0;
 
+    return () => {
+        const toReturn = arr[index];
+        index += 1;
+        return toReturn;
+    };
+};
+const test = [
+    1, 2, 7, 11
+    //2, 1, 5, 3
+    // 3, -2, -1, 4
+];
+const gets = this.gets || getGets(test);
+const print = this.print || console.log;
+solve(gets);
 
-    // console.log(bricks);
-    printer(bricks);
+function solve(getParams) {
+    let args = [];
+    for (let i = 0; i < 4; i++) {
+        args.push(Number(getParams()));
+    }
 
-    function printer(obj) {
-        for (let i = 0; i < obj.length; i++) {
-            let print = obj[i].join(" ");
-            console.log(print);
+    let b1 = Number(args[0]);
+    let b2 = Number(args[1]);
+    let b3 = Number(args[2]);
+    let layers = Number(args[3]);
+
+    let triangle = [];
+    triangle[0] = [b1];
+    triangle[1] = [b2, b3];
+
+    let holder = [b1, b2, b3];
+    for (let i = 2; i < layers; i++) {
+        let bricksInLayerCounter = i + 1;
+        triangle[i] = [];
+        for (let brick = 0; brick < bricksInLayerCounter; brick++) {
+            triangle[i].push(HolderSum(holder)); //check
+            // triangle[i].push(holder.sum());//check
+            holder = HolderReSum(holder);
         }
     }
 
-    function builder() {
+    for (let line of triangle) {
+        let sb = line.join(" ") + "\n";
+        console.log(sb.trim());
+    }
 
+    function HolderReSum(holder) {
+        let sum = HolderSum(holder);
+        holder[0] = holder[1];
+        holder[1] = holder[2];
+        holder[2] = sum;
+        return holder;
+    }
+
+    function HolderSum(holder) {
+        let sum = holder[0] + holder[1] + holder[2];
+        return sum;
     }
 }
-
-solve(
-    [2, 1, 5, 3]
-);
